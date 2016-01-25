2ded4c8b8508e751db44343877b81bac8f0d5918
chenxuejia <chenxuejia@idcos.com>
2016-01-25 15:29:54 +0800

    weibo auth provider

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -36,6 +36,12 @@ Route::get('/categories/{cid?}', [
     'uses' => 'PagesController@showCategoriesProducts'
 ]);
 
+// 引导用户到新浪微博的登录授权页面
+Route::get('auth/weibo', 'Auth\AuthController@weibo');
+
+// 用户授权后新浪微博回调的页面
+Route::get('auth/callback', 'Auth\AuthController@callback');
+
 // Route::get('/product/{pid?}', function($id="1") {
 // 	return 'product'.$id;
 // });
